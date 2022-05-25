05762434e806c059a5c0e9b52d73ced689bdaf63
manu <manu@local.com>
2022-05-25 16:47:11 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -14,7 +14,7 @@ resource "aws_lb_target_group_attachment" "instance-attachment" {
 
 resource "aws_lb_listener_rule" "app_rule" {
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  priority     = random_integer.priority.result
 
   action {
     type             = "forward"
@@ -27,3 +27,11 @@ resource "aws_lb_listener_rule" "app_rule" {
     }
   }
 }
+
+
+resource "random_integer" "priority" {
+  min = 101
+  max = 500
+}
+
+
