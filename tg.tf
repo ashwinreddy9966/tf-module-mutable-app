02f67eb26a6f378c96f66223718ea84ec353a509
manu <manu@local.com>
2022-05-25 14:55:30 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -2,5 +2,11 @@ resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   =data.terraform_remote_state.vpc.outputs.VPC_ID 
+  vpc_id   =data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.test.id
+  port             = 80
 }
