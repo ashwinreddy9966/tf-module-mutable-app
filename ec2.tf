aa7b7838b8afc85183416b0fb273834d04388543
manu <manu@local.com>
2022-05-25 12:58:12 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_spot_instance_request" "spot" {
+  count                 = var.SPOT_INSTANCE_COUNT
   ami                   = data.aws_ami.ami
   instance_type         = var.INSTANCE_TYPE
-  wait_for_fulfillment  =  = true
+  wait_for_fulfillment  = true
 
   tags = {
     Name = "CheapWorker"
