930255241f51c2bfb672a1f3d5002bb46fb66b87
manu <manu@local.com>
2022-05-25 13:46:04 +0530

    Auto Push Commit

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   ALL_INSTANCE_IDS = concat(aws_instance.OD.*.id, aws_spot_instance_request.spot.*.spot_instance_id)
-  ALL_INSTANCE_PRIVATE_IPS = concat(aws_instance.OD.*.private_ip , aws_instance.OD.*.private_ip
+  ALL_INSTANCE_PRIVATE_IPS = concat(aws_instance.OD.*.private_ip ,aws_spot_instance_request.spot.*.private_ip)
   }
