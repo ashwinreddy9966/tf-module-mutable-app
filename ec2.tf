097e568f4890ca9fdb3dd4ddb5db9b8acaec5f09
manu <manu@local.com>
2022-05-25 13:06:30 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_spot_instance_request" "spot" {
-  count                 = var.SPOT_INSTANCE_COUNT
-  ami                   = data.aws_ami.ami
-  instance_type         = var.INSTANCE_TYPE
-  wait_for_fulfillment  = true
+  count                   = var.SPOT_INSTANCE_COUNT
+  ami                     = data.aws_ami.ami
+  instance_type           = var.INSTANCE_TYPE
+  wait_for_fulfillment    = true
+  vpc_security_group_ids  = [aws_security_group.allows_app.id]
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
