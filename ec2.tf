d70ae2363f1581770913280e5274cd01c767ba0b
manu <manu@local.com>
2022-05-25 13:01:09 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -9,3 +9,12 @@ resource "aws_spot_instance_request" "spot" {
   }
 }
 
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
