ccc00795287241fd373c32694154e486c9226e34
manu <manu@local.com>
2022-05-25 19:20:40 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -5,8 +5,13 @@ resource "aws_lb_target_group" "app" {
   vpc_id   =data.terraform_remote_state.vpc.outputs.VPC_ID
 
   health_check {
-    enabled = true
-    
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    timeout             = 4
+    unhealthy_threshold = 2
+
   }
 
 }
