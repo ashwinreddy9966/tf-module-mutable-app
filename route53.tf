f14f27f38fee8e8acdbb9beca10b8c7a8fb42041
manu <manu@local.com>
2022-05-25 16:04:32 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "record" {
   name    = "${var.COMPONENT}-${ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_NAME}"
   type    = "A"
   ttl     = "60"
-  records = var.LB_TYPE ? internal [data.terraform_remote_state.alb.outputs.ALB_ADDRESS]
+  records = var.LB_TYPE == internal ? [data.terraform_remote_state.alb.outputs.ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.ALB_ADDRESS]
 }
