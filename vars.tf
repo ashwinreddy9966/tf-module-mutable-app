554c12458a723b60aa2c7c6d660d3cd15a726f38
manu <manu@local.com>
2022-06-05 11:19:00 +0530

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -4,6 +4,7 @@ variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
 variable "WORKSTATION_IP" {}
+variable "PROMETHEUS_IP" {}
 variable "APP_PORT" {}
 variable "APP_VERSION" {}
 variable "LB_TYPE" {}
